31484c0194647722029f3467d48b80e9708eebd8
Boddu <1895582@MATRIXCOLLEGE.LCL>
2020-01-18 17:15:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/firebase_practice/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.firebase_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button LoginGoo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(getApplicationContext(),Login.class));
+
+
+
+    }
+}
